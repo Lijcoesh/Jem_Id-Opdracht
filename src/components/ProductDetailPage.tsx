6cba55476ba5e4d3c78612f1af254b8d872bdc3f
Lijcoesh <lucanoort@gmail.com>
2025-05-04 16:52:37 +0200

    productDetailPagina (niet werkend + niet compleet)

--- src/components/ProductDetailPage.tsx
+++ src/components/ProductDetailPage.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Product, StandingPlace, WateringNeeds } from "../types/Product";
 
 const ProductDetailPage: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [product, setProduct] = useState<Product | null>(null);
   const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    const foundProduct = products.find((p) => p.id === Number(id));
+    if (foundProduct) {
+      setProduct(foundProduct);
+    }
+  }, [id, products]);
+
+  if (!product) {
+    return <div>Loading product...</div>;
+  }
 
   return (
-    <div className="product-detail-container">
-      <h1>Product Detail</h1>
-      <p>Product ID: {id}</p>
+    <div>
+      <h1>Product detail</h1>
+      <div>
+        <img src={product.photoUrl} alt={product.name} />
+
+        <div>
+          <h2>{product.name}</h2>
+          <p>€{product.price}</p>
+          <p>{product.description}</p>
+          <p>Product ID: {id}</p>
+        </div>
+      </div>
     </div>
   );
 };
